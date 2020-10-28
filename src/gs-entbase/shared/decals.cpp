327bc0e696b1bacf6806490cf9fd57399f6de3bf
WatIsDeze <watisdeze2@gmail.com>
2020-10-28 14:28:47 +0100

    Added FreeCS commit: 0f50c46

--- src/gs-entbase/shared/decals.cpp
+++ src/gs-entbase/shared/decals.cpp
@@ -56,8 +56,6 @@ decal::ReceiveEntity(void)
 	angles[0] = readcoord();
 	angles[1] = readcoord();
 	angles[2] = readcoord();
-	setorigin(this, origin);
-
 	m_strTexture = readstring();
 
 	size = drawgetimagesize(m_strTexture);
@@ -79,11 +77,6 @@ decal::ReceiveEntity(void)
 float
 decal::predraw(void)
 {
-	vector vecPlayer;
-	int s = (float)getproperty(VF_ACTIVESEAT);
-	pSeat = &g_seats[s];
-	vecPlayer = pSeat->m_vecPredictedOrigin;
-
 	decal dcl = (decal)self;
 	if (!autocvar_r_drawdecals) {
 		return PREDRAW_NEXT;
@@ -94,9 +87,11 @@ decal::predraw(void)
 		return PREDRAW_NEXT;
 
 	/* don't draw us, unnecessary */
-	if (checkpvs(vecPlayer, this) == FALSE) {
+	/*if (checkpvs(getproperty(VF_ORIGIN), this) == FALSE) {
 		return PREDRAW_NEXT;
-	}
+	}*/
+
+	
 
 	adddecal(dcl.m_strShader, dcl.origin, dcl.mins, dcl.maxs, dcl.color, 1.0f);
 	addentity(dcl);
@@ -133,7 +128,6 @@ decal::Place(vector org, string dname)
 	}
 
 	origin = g_tracedDecal.endpos;
-	setorigin(this, origin);
 
 	/* FIXME: more universal check? */
 	if (getsurfacetexture(trace_ent, getsurfacenearpoint(trace_ent, g_tracedDecal.endpos)) == "sky") {
@@ -234,10 +228,6 @@ void Decals_Place(vector pos, string dname)
 void
 Decal_Reload(void)
 {
-	for (entity b = world; (b = find(b, ::classname, "tempdecal"));) {
-		decal d = (decal)b;
-		d.BuildShader();
-	}
 	for (entity b = world; (b = find(b, ::classname, "decal"));) {
 		decal d = (decal)b;
 		d.BuildShader();
@@ -272,5 +262,4 @@ decal::decal(void)
 {
 	m_strShader = __NULL__;
 	m_strTexture = __NULL__;
-	setsize(this, [0,0,0], [0,0,0]);
 }
