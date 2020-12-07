bcd99c1267b4e94eb8cfdb225f87646ed980aae2
WatIsDeze <watisdeze2@gmail.com>
2020-12-07 22:15:24 +0100

    Added buggy, but working drawers to the bedroom, pistol resides there.

--- src/server/schizomania/entities/item_inventory.cpp
+++ src/server/schizomania/entities/item_inventory.cpp
@@ -123,8 +123,9 @@ void item_inventory::Respawn(void) {
 	// Set defaults.
 	SetModel(g_inventory_items[m_iItemID].wmodel());
 	SetGFlags(gflags | GF_HOVER_FULLBRIGHT);
-	SetSize([-8,-8,0], [8,8,16]);
+	SetSize([-8,-8,0], [8,8,7]);
 	SetSolid(SOLID_CORPSE);
+	self.solid = SOLID_CORPSE;
 	SetMovetype(MOVETYPE_TOSS);
 
 	// Set PlayerUse
